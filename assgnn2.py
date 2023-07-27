7fabcab1bf4908de34cd2fc978ca4d0c4c6f0108
caainat <132391501+caainat@users.noreply.github.com>
2023-07-27 19:31:01 +0530

    Create assgnn2.py

---
+++ assgnn2.py
@@ -0,0 +1,86 @@
+#ans 1
+#this is how we comment
+# this is a single line comment
+"""
+this
+is
+a
+multiple
+linecomment
+"""
+
+#ans 2
+a= 123
+b= " KAINAT NAQVI"
+d= ' kainat'
+# variables are used to store data values. 
+type(a)
+
+#ans 3 
+e= 45
+f= str(e)
+f
+#this is how you convert int to string
+
+#ans 4
+"""
+Write the Script:
+
+write your Python code in it and save the file with a .py extension, like myscript.py.
+
+Open the Command Line:
+
+On Windows, open the Command Prompt or PowerShell.
+On macOS or Linux, open the Terminal.
+Navigate to the File's Location:
+
+Use the cd command to go to the folder where the script is saved.
+Run the Script:
+
+Type python myscript.py (replace myscript.py with your file name) and press Enter.
+See the Output:
+
+The Python script will run, and you'll see the output in the command line.
+"""
+
+a1 = " 456"
+a1 = int(a1)
+# converting string into int
+type(a1)
+a1
+
+#ans 5
+my_list = [1, 2, 3, 4, 5]
+a5 = my_list[ 1:3]
+a5
+
+#ans 6
+# complex numbers are number which have imaginary as well as real number in it
+# fr ex = 34 + 5 i
+# in python imaginary part is representd by j
+b2 = 34+ 5j
+
+
+#ans7
+age= 25
+age
+
+#ans 8 
+price=9.99
+type(price)
+
+#ans 9
+name= " kainat naqvi"
+print("my name is " , name)
+
+#ans 10
+str1= "Hello, World!"
+str2 = str1[7:12]
+print(str2)
+
+#ans 11 
+is_student= True
+is_student
+
+
+
